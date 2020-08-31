01670eda135a949e05d2256135340cf0eb45902f
tiff-19 <tiffany.tl94@gmail.com>
2020-08-31 21:07:16 +0700

    fix 1st

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "google" {
-  credentials = file("/mnt/c/Users/Tiffany/Downloads/authentic-scout-282804-ca5449a8b399.json")
-  project = "Purwadhika-Lab"
+  credentials = file("authentic-scout-282804-ca5449a8b399.json")
+  project = "authentic-scout-282804"
   region  = "asia-southeast2"
   zone    = "asia-southeast2-b"
 }
--- vpc.tf
+++
@@ -1,4 +0,0 @@
-resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = "true"
-}

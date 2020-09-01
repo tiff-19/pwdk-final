9b1b22b6c318864f04b4f7d6e5e9e82be602a4cd
tiff-19 <tiffany.tl94@gmail.com>
2020-09-01 21:36:16 +0700

    2nd

--- instance.tf
+++ instance.tf
@@ -17,3 +17,23 @@ resource "google_compute_instance" "final" {
     }
   }
 }
+
+resource "google_compute_instance" "final-node" {
+  name         = "final-node"
+  machine_type = "n1-standard-1"
+  zone         = "asia-southeast2-b"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
